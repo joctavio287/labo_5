67c8fba22c82f02db7739631b6183260d4bf5c0c
joctavio287 <joctavio287@gmail.com>
2023-02-03 19:31:19 -0300

    me confundi donde guardaba las pics

--- espectroscopia/analisis_espectroscopioa.py
+++ espectroscopia/analisis_espectroscopioa.py
@@ -17,7 +17,7 @@ ax.set_xlabel('Corriente controlador láser [mA]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
 fig.show()
-# fig.savefig(fname = os.path.join(input_path + os.path.normpath('/medicion_sinrb.png')))
+# fig.savefig(fname = os.path.join(output_path + os.path.normpath('/medicion_sinrb.png')))
 
 # Grafico tension del fotodiodo (V) vs corriente del controlador (mA). Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/medicion_conrb.pkl')), mode = "rb") as archive:
@@ -29,7 +29,7 @@ ax.set_xlabel('Corriente controlador láser [mA]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
 fig.show()
-# fig.savefig(fname =os.path.join(input_path + os.path.normpath('/medicion_conrb.png')))
+# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_conrb.png')))
 
 # Grafico tension del fotodiodo (V) vs temperatura del controlador (ºC). Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/medicion_1200uA.pkl')), mode = "rb") as archive:
@@ -41,7 +41,7 @@ ax.set_xlabel(r'Corriente controlador láser [$^{o}C$]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
 fig.show()
-# fig.savefig(fname =os.path.join(input_path + os.path.normpath('/medicion_1200uA.png')))
+# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_1200uA.png')))
 
 # Captura del canal 1 del osciloscopio a 24.126 ºC y modulado de rampa de 1Hz. Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/absorcion_24_126C.pkl')), mode = "rb") as archive:
@@ -53,4 +53,4 @@ ax.set_xlabel('Tiempo [s]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
 fig.show()
-# fig.savefig(fname =os.path.join(input_path + os.path.normpath('/absorcion_24_126C.png')))
+# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/absorcion_24_126C.png')))

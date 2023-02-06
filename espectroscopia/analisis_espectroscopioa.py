5961664f6d1075bc876a8f88db19c8cea00d1e8e
joctavio287 <joctavio287@gmail.com>
2023-02-06 18:06:48 -0300

    agregue errores a los graficos

--- espectroscopia/analisis_espectroscopioa.py
+++ espectroscopia/analisis_espectroscopioa.py
@@ -12,7 +12,7 @@ with open(file = os.path.join(input_path + os.path.normpath('/medicion_sinrb.pkl
     datos_sinrb = pickle.load(file = archive)
 
 fig, ax = plt.subplots(nrows = 1, ncols = 1)
-ax.scatter(datos_sinrb['corriente_setting'], datos_sinrb['tension'], s = 15)
+ax.scatter(datos_sinrb['corriente_setting'], datos_sinrb['tension'], s = 5, color = 'black', label = 'Datos')
 ax.errorbar(
 datos_sinrb['corriente_setting'], 
 datos_sinrb['tension'],
@@ -25,41 +25,73 @@ label = 'Error de los datos')
 ax.set_xlabel('Corriente controlador láser [mA]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
-fig.show()
-# fig.savefig(fname = os.path.join(output_path + os.path.normpath('/medicion_sinrb.png')))
+ax.legend()
+# fig.show()
+fig.savefig(fname = os.path.join(output_path + os.path.normpath('/medicion_sinrb.png')))
 
 # Grafico tension del fotodiodo (V) vs corriente del controlador (mA). Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/medicion_conrb.pkl')), mode = "rb") as archive:
     datos_conrb = pickle.load(file = archive)
 
 fig, ax = plt.subplots(nrows = 1, ncols = 1)
-ax.scatter(datos_conrb['corriente_setting'], datos_conrb['tension'], s = 15)
+ax.scatter(datos_conrb['corriente_setting'], datos_conrb['tension'], s = 5, color = 'black', label = 'Datos')
+ax.errorbar(
+datos_conrb['corriente_setting'], 
+datos_conrb['tension'],
+yerr = 2*(5*5/256), #(escala*divisiones/resolución)*2 (*2 pq asignamos el 200 porciento debido a ruido)
+marker = '.', 
+fmt = 'None', 
+capsize = 2, 
+color = 'black', 
+label = 'Error de los datos')
 ax.set_xlabel('Corriente controlador láser [mA]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
-fig.show()
-# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_conrb.png')))
+ax.legend()
+# fig.show()
+fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_conrb.png')))
 
 # Grafico tension del fotodiodo (V) vs temperatura del controlador (ºC). Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/medicion_1200uA.pkl')), mode = "rb") as archive:
     datos_1200uA = pickle.load(file = archive)
 
 fig, ax = plt.subplots(nrows = 1, ncols = 1)
-ax.scatter(datos_1200uA['temperatura'], datos_1200uA['tension'], s = 15)
+ax.scatter(datos_1200uA['temperatura'], datos_1200uA['tension'], s = 5, color = 'black', label = 'Datos')
+ax.errorbar(
+datos_1200uA['temperatura'], 
+datos_1200uA['tension'],
+yerr = 2*(5*5/256), #(escala*divisiones/resolución)*2 (*2 pq asignamos el 200 porciento debido a ruido)
+marker = '.', 
+fmt = 'None', 
+capsize = 2, 
+color = 'black', 
+label = 'Error de los datos')
 ax.set_xlabel(r'Corriente controlador láser [$^{o}C$]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
-fig.show()
-# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_1200uA.png')))
+ax.legend()
+# fig.show()
+fig.savefig(fname =os.path.join(output_path + os.path.normpath('/medicion_1200uA.png')))
 
 # Captura del canal 1 del osciloscopio a 24.126 ºC y modulado de rampa de 1Hz. Con paso por rubidio
 with open(file = os.path.join(input_path + os.path.normpath('/absorcion_24_126C.pkl')), mode = "rb") as archive:
     datos_24_126C = pickle.load(file = archive)
 
 fig, ax = plt.subplots(nrows = 1, ncols = 1)
-ax.scatter(datos_24_126C['tiempo'], datos_24_126C['tension'], s = 2)
+ax.scatter(datos_24_126C['tiempo'][1230:1930], datos_24_126C['tension'][1230:1930], s = 5, color = 'black', label = 'Datos')
+# ax.errorbar(
+# datos_24_126C['tiempo'][1230:1930], 
+# datos_24_126C['tension'][1230:1930],
+# yerr = 2*(5*5/256), #(escala*divisiones/resolución)*2 (*2 pq asignamos el 200 porciento debido a ruido)
+# marker = '.', 
+# fmt = 'None', 
+# capsize = .2, 
+# color = 'black', 
+# label = 'Error de los datos')
 ax.set_xlabel('Tiempo [s]')
 ax.set_ylabel('Tensión de fotodiodo [V]')
 ax.grid(visible = True)
-fig.show()
-# fig.savefig(fname =os.path.join(output_path + os.path.normpath('/absorcion_24_126C.png')))
+ax.legend()
+# fig.show()
+fig.savefig(fname =os.path.join(output_path + os.path.normpath('/absorcion_24_126C.png')))
+

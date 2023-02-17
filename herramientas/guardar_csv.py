cb502dea273f97fc16b0efcbcac5d2fdd4c971b5
Javi Tau <javitau459@gmail.com>
2023-02-16 22:34:14 -0300

    Agrego una advertencia mas por si se sobreescribe el archivo

--- herramientas/guardar_csv.py
+++ herramientas/guardar_csv.py
@@ -7,5 +7,7 @@ def guardar_csv(*data, filename, root='.\\', delimiter=',', header='', rewrite=F
     if not rewrite and isfile:
         print('Este archivo ya existe, para sobreescribirlo usar el argumento rewrite')
         return
+    if isfile:
+        print('ATENCIÓN: SE SOBREESCRIBIÓ EL ARCHIVO')
     np.savetxt(root+filename+'.csv', np.transpose(np.array([*data])), header=header, delimiter=delimiter)
     return

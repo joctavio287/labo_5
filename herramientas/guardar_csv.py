1cfba586a4091bf35b060eaa3aca61b2c31d1d34
Javi Tau <javitau459@gmail.com>
2023-02-16 22:24:25 -0300

    Agrego funcion para guardar csv

---
+++ herramientas/guardar_csv.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def guardar_csv(*data, filename, root='.\\', delimiter=',', header='', rewrite=False):
+    isfile = os.path.isfile(root+filename+'.csv')
+    if not rewrite and isfile:
+        print('Este archivo ya existe, para sobreescribirlo usar el argumento rewrite')
+        return
+    np.savetxt(root+filename+'.csv', np.transpose(np.array([*data])), header=header, delimiter=delimiter)
+    return

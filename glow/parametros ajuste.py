04f82473e4fc1c28bb0ee45c9463f441ea8887a7
joctavio287 <joctavio287@gmail.com>
2023-03-10 19:16:07 -0300

    AJUPSTE PASCHEN

--- glow/parametros ajuste.py
+++ glow/parametros ajuste.py
@@ -24,9 +24,11 @@ for i in range(1, 26):
 rupturas = np.array(rupturas).reshape(-1,1)-3
 pds = np.array(pds).reshape(-1,1)
 
-a_0, a_1, a_2, p_d = symbols("a_0 a_1 a_2 p_d",   real = True)
-expr = a_0*(p_d)/(log(a_1*p_d) + a_2) 
-# expr = (p_d/.49)*(a_0 + a_1*.49)/(log(a_2*p_d))
+a_0, a_1, p_d = symbols("a_0 a_1 p_d",   real = True)
+# def func(x, a_0, a_1):
+#     return a_0*(x)/(np.log(x/a_1) + 1)
+
+expr = 'a_0*(p_d)/(log(p_d/a_1) + 1)'
 
 pds_auxiliar = np.linspace(pds[0], pds[-1],1000)
 
@@ -39,13 +41,13 @@ with plt.style.context('seaborn-whitegrid'):
     
     # Valores iniciales
     # A_0, A_1, A_2 = 555, .0000482, 11.458
-    A_0, A_1, A_2 = 365, .0000482, 11.458
+    A_0, A_1 = 500, .63
     # A_0, A_1, A_2  = 699, 0.001, 12
     # A_0, A_1 = 555, .0000482
  
     # CAmbia de sympy a numpy
-    lam_x = lambdify([p_d, a_0, a_1, a_2], expr, modules=['numpy'])
-    l, = plt.plot(pds_auxiliar, lam_x(pds_auxiliar, A_0, A_1, A_2), linewidth = 2)
+    lam_x = lambdify([p_d, a_0, a_1], expr, modules=['numpy'])
+    l, = plt.plot(pds_auxiliar, lam_x(pds_auxiliar, A_0, A_1), linewidth = 2)
     
     ax.margins(x = 0)
     ax.set_xlim([0, 2])
@@ -56,22 +58,22 @@ with plt.style.context('seaborn-whitegrid'):
     axcolor = 'lightgoldenrodyellow'
     ax_0 = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor = axcolor) # left, bottom, width, height
     ax_1 = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor = axcolor)
-    ax_2 = plt.axes([0.25, 0.25, 0.65, 0.03], facecolor = axcolor)
+    # ax_2 = plt.axes([0.25, 0.25, 0.65, 0.03], facecolor = axcolor)
 
     # See documentation of Slider to get configurations
     s_0 = Slider(ax_0, label = 'a_0', valmin = 100, valmax = 800, valinit = A_0, valstep = 1)
-    s_1 = Slider(ax_1, label = 'a_1', valmin = .0000001, valmax = .001, valinit = A_1, valstep = .0000001)
-    s_2 = Slider(ax_2, label = 'a_2', valmin = 2, valmax = 20, valinit = A_2, valstep = .001)
+    s_1 = Slider(ax_1, label = 'a_1', valmin = .5, valmax = .75, valinit = A_1, valstep = .001)
+    # s_2 = Slider(ax_2, label = 'a_2', valmin = 2, valmax = 20, valinit = A_2, valstep = .001)
 
     def update(val):
         a_0 = s_0.val
         a_1 = s_1.val
-        a_2 = s_2.val
-        l.set_ydata(lam_x(pds_auxiliar, a_0, a_1, a_2))
+        # a_2 = s_2.val
+        l.set_ydata(lam_x(pds_auxiliar, a_0, a_1))#, a_2))
         fig.canvas.draw_idle()
     s_0.on_changed(update)
     s_1.on_changed(update)
-    s_2.on_changed(update)
+    # s_2.on_changed(update)
 
     resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
     button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
@@ -79,7 +81,7 @@ with plt.style.context('seaborn-whitegrid'):
     def reset(event):
         s_0.reset()
         s_1.reset()
-        s_2.reset()
+        # s_2.reset()
     button.on_clicked(reset)
     # rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
     # radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)

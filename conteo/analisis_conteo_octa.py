d67483aa1a166c131be4db567c617d5399c18f20
joctavio287 <joctavio287@gmail.com>
2023-03-16 10:31:03 -0300

    analizando en detalle distr

--- conteo/analisis_conteo_octa.py
+++ conteo/analisis_conteo_octa.py
@@ -52,75 +52,60 @@ def definir_frecuencia(tension:float):
     '''
     return (tension - 1.6922156274324787)/.35639131796658835
 
-# =================
-# Fuente no térmica
-# =================
 
-# Determinamos el umbral
+# ====================
+# Distribución Poisson
+# ====================
 
-# Definición del umbral
-paso = 2*.5*10/256 # medicion['unidades'].split('CH1: ')[1].split(';')[0]
-maximo_altura = .02
-bins = np.arange(-maximo_altura, 0, paso)
+# Definición del umbral mediante método visual
 
 # Laser
 tensiones = []
-carpeta = '/poisson(10ms)/laser_2v/'
-# carpeta = '/viejo/laser_prendido_4/'
+carpeta = '/poisson(10ms)/laser_2v_bis_2/'
 dic = {}
 for f in os.listdir(os.path.join(input_path + carpeta)):
     medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
-    # medicion['unidades'] = 'Reescribimos el umbral en 0.' +medicion['unidades']
-    # medicion['indice_picos'] = find_peaks(-medicion['tension'], height = 0)[0]#, distance = 50)[0]
-    # medicion['indice_picos'] = medicion['indice_picos'][medicion['tension'][medicion['indice_picos']]<0]
-    # medicion['tiempo_picos'] = medicion['tiempo'][medicion['indice_picos']]
-    # medicion['tension_picos'] = medicion['tension'][medicion['indice_picos']]
+    for t in medicion['tension_picos']:
+        tensiones.append(t)
     
-    # save_dict(os.path.join(input_path + carpeta + f), medicion, rewrite = True)
-    tensiones.append(medicion['tension_picos'].reshape(-1,1))
-    # tensiones_1, cuentas = np.unique(medicion['tension_picos'], return_counts = True)
-    # for te, cuen in zip(tensiones_1, cuentas):
-    #     try:
-    #         dic[te] = cuen
-    #     except:
-    #         dic[te] += cuen
-
-tensiones = np.concatenate(tensiones, axis = 0)
+tensiones, cuentas = np.unique(tensiones, return_counts = True)
 
 # Ruido
 tensiones_ruido = []
 carpeta = '/poisson(10ms)/ruido_2v/'
 for f in os.listdir(os.path.join(input_path + carpeta)):
     medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
-    tensiones_ruido.append(medicion['tension_picos'].reshape(-1,1))
-tensiones_ruido = np.concatenate(tensiones_ruido, axis = 0)
+    for t in medicion['tension_picos']:
+        tensiones_ruido.append(t)
+tensiones_ruido, cuentas_ruido = np.unique(tensiones_ruido, return_counts = True)
 
+# Graficamos definiendo un umbral
+umbral = 3.2 #tensiones_ruido.mean() - 3*np.std(tensiones_ruido)
 plt.figure()
-# plt.hist(tensiones_ruido,
-#           bins = bins,
-#           label = "Ruido",
-#           histtype = "step", 
-#           color = "green")
-# plt.vlines(umbral, 0, 80000, color = 'red', label = f'umbral: {umbral}')
-# cuentas, frec 
-# plt.bar(dic.keys(),
-#         dic.values(),
-#         color = 'C0', 
-#         width=.015)
-plt.hist(tensiones,
-          bins = bins,
-          label = "Laser prendido",
-          histtype = "step", 
-          color = "C1")
-plt.legend()
+
+plt.vlines(x = -umbral, ymin = 0, ymax =100000, label = f'Umbral:{-umbral}', color = 'C2', linestyles='dashed')
+plt.bar(tensiones,
+        cuentas,
+        color = 'C1', 
+        width=.15,
+        alpha = .75,
+        label = 'Laser incidiendo sobre FM')
+plt.bar(tensiones_ruido,
+        cuentas_ruido,
+        color = 'C0', 
+        width= .15,
+        alpha = 1,
+        label = 'Ruido')
+# plt.ylim(0,cuentas_ruido.max())
 plt.xlabel('Tensión [V]')
 plt.ylabel('Número de eventos')
 plt.grid(visible = True, alpha=0.3)
 plt.yscale('log')
+plt.legend()
 plt.show(block = False)
 
-# Hacemos las cuentas habiendo fijado el umbral
-umbral = 2 #tensiones_ruido.mean() - 3*np.std(tensiones_ruido)
+# Levantamos la estadística habiendo fijado el umbral
+carpeta = '/poisson(10ms)/laser_2v_bis_2/'
 ocurrencias = []
 for f in os.listdir(os.path.join(input_path + carpeta)):
     medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
@@ -137,109 +122,61 @@ for f in os.listdir(os.path.join(input_path + carpeta)):
     #     plt.show(block = False)
     ocurrencias.append(ocurrencia)
 
-# ocurrencias = np.array(ocurrencias)
-# cuentas, bordes = np.histogram(ocurrencias)
-cuentas, frecuencia = np.unique(ocurrencias, return_counts = True)
-frecuencia = frecuencia
+# Hacemos un ajuste
+cuentas, apariciones = np.unique(ocurrencias, return_counts = True)
+error_suma_apariciones = np.sqrt(np.sum(apariciones))
+frecuencia = apariciones/np.sum(apariciones)
+error_frecuencia = np.sqrt((np.sqrt(apariciones)/np.sum(apariciones))**2 + 
+        (apariciones*error_suma_apariciones/(np.sum(apariciones)**2))**2
+        )
+
+
+# Valor medio y error
+media = np.array([cuenta*frec for cuenta, frec in zip(cuentas, frecuencia)]).sum()
+media_std = np.sqrt(np.sum([(cuenta*e_frec)**2 for cuenta, e_frec in zip(cuentas, error_frecuencia)]))
+factorial = lambda x: for y in x np.math.factorial(y) 
+from scipy import math.factorial as factorial
+
+factorial(np.array([1,2,3]))
+formula_bose = 'a_0**x/((1+a_0)**(1+x))'
+def formula_bose_f(a_0, x):
+    return a_0**x/((1+a_0)**(1+x))
+
+franja_error = Propagacion_errores(
+        variables = [('a_0', media)], 
+        errores = np.array([media_std]).reshape(-1,1), 
+        formula = formula_bose, 
+        dominio = cuentas
+        ).fit()[1]
+
 
 plt.figure()
-# plt.stairs(cuentas, bordes, fill=True)
-# plt.vlines(bordes, 0, cuentas.max(), colors='w')
-plt.bar(cuentas, frecuencia)
+plt.bar(cuentas,
+        frecuencia,
+        color = 'C0', 
+        width= .5,
+        alpha = 1)
+plt.xlabel('Número de fotones')
+plt.ylabel('Ocurrencias')
+plt.grid(visible = True, alpha=0.3)
+plt.show(block = False)
+plt.figure()
+
+plt.errorbar(cuentas, frecuencia, yerr = error_frecuencia, fmt = 'o', capsize = 1.5, color = 'C1', label = 'Experimental')
+# plt.scatter(cuentas, frecuencia, marker = '.', fmt = 'None', capsize = 1.5, color = 'black', label = 'Experimental')
+plt.bar(cuentas, frecuencia, color = 'C2')
+
+plt.errorbar(cuentas, formula_bose_f(media, cuentas), yerr = franja_error, marker = 'o', capsize = 1.5, color = 'C0', label = 'Ajuste')
+# plt.plot(cuentas, formula_bose_f(media, cuentas), 'r.-', label = 'Ajuste', alpha = .5)
+# plt.plot(cuentas, formula_bose_f(media, cuentas) + franja_error, '--', color = 'green', label = 'Error del ajuste')
+# plt.plot(cuentas, formula_bose_f(media, cuentas) - franja_error, '--', color = 'green')
+# plt.fill_between(cuentas, formula_bose_f(media, cuentas) - franja_error, formula_bose_f(media, cuentas) + franja_error, facecolor = "gray", alpha = 0.3)
+
 plt.xlabel('Número de fotones')
 plt.ylabel('Ocurrencia')
 plt.grid(visible = True, alpha=0.3)
-plt.show(block = False)
-
-# ====================
-# Distribución Poisson
-# ====================
-
-# Determinamos el umbral
-
-# Definición del umbral
-paso = 2*.5*10/256 # medicion['unidades'].split('CH1: ')[1].split(';')[0]
-maximo_altura =6
-bins = np.arange(-maximo_altura, 0, paso)
-
-# Laser
-tensiones = []
-carpeta = '/poisson(10ms)/laser_2v_bis/'
-
-for f in os.listdir(os.path.join(input_path + carpeta)):
-    medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
-    # medicion['unidades'] = 'Reescribimos el umbral en 0.' +medicion['unidades']
-    # medicion['indice_picos'] = find_peaks(-medicion['tension'], height = 0)[0]#, distance = 50)[0]
-    # medicion['indice_picos'] = medicion['indice_picos'][medicion['tension'][medicion['indice_picos']]<0]
-    # medicion['tiempo_picos'] = medicion['tiempo'][medicion['indice_picos']]
-    # medicion['tension_picos'] = medicion['tension'][medicion['indice_picos']]
-    
-    # save_dict(os.path.join(input_path + carpeta + f), medicion, rewrite = True)
-    tensiones.append(medicion['tension_picos'].reshape(-1,1))
-    # tensiones.append(medicion['tension'].reshape(-1,1))
-
-tensiones = np.concatenate(tensiones, axis = 0)
-
-# # Ruido
-# tensiones_ruido = []
-# carpeta = '/poisson(10ms)/ruido_2v/'
-# for f in os.listdir(os.path.join(input_path + carpeta)):
-#     medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
-#     tensiones_ruido.append(medicion['tension_picos'].reshape(-1,1))
-# tensiones_ruido = np.concatenate(tensiones_ruido, axis = 0)
-
-plt.figure()
-# plt.hist(tensiones_ruido,
-#           bins = bins,
-#           label = "Ruido",
-#           histtype = "step", 
-#           color = "green")
-# plt.vlines(umbral, 0, 80000, color = 'red', label = f'umbral: {umbral}')
-
-plt.hist(tensiones,
-          bins = bins,
-          label = "Laser prendido",
-          histtype = "step", 
-          color = "blue")
 plt.legend()
-plt.xlabel('Tensión [V]')
-plt.ylabel('Número de eventos')
-plt.grid(visible = True, alpha=0.3)
-plt.yscale('log')
 plt.show(block = False)
-
-# Hacemos las cuentas habiendo fijado el umbral
-umbral = 2 #tensiones_ruido.mean() - 3*np.std(tensiones_ruido)
-ocurrencias = []
-for f in os.listdir(os.path.join(input_path + carpeta)):
-    medicion = load_dict(fname = os.path.join(input_path + carpeta + f))
-
-    # Sacamos los indices   
-    indice_picos = find_peaks(-medicion['tension'], height = umbral, distance = 10)[0]
-    tension_picos = medicion['tension'][indice_picos]
-    ocurrencia = len(tension_picos[tension_picos<-umbral])
-
-    # if ocurrencia>0:
-    #     plt.figure()
-    #     plt.plot(medicion['tiempo'], medicion['tension'],'o-', markersize =1)
-    #     plt.scatter(medicion['tiempo'][indice_picos], medicion['tension'][indice_picos], color = 'red')
-    #     plt.show(block = False)
-    ocurrencias.append(ocurrencia)
-
-# ocurrencias = np.array(ocurrencias)
-# cuentas, bordes = np.histogram(ocurrencias)
-cuentas, frecuencia = np.unique(ocurrencias, return_counts = True)
-frecuencia = frecuencia
-
-plt.figure()
-# plt.stairs(cuentas, bordes, fill=True)
-# plt.vlines(bordes, 0, cuentas.max(), colors='w')
-plt.bar(cuentas, frecuencia)
-plt.xlabel('Número de fotones')
-plt.ylabel('Ocurrencia')
-plt.grid(visible = True, alpha=0.3)
-plt.show(block = False)
-
 # ==================
 # Distribución Bose
 # ==================

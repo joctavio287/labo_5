11506d444dc5fe2abe8b32dc536c10979ec2d76c
Javi Tau <javitau459@gmail.com>
2023-03-16 17:59:21 -0300

    agrego todos los graficos de P0,1,2 y arreglo el del ejemplo del ruido

--- conteo/analisis_conteo_javi.py
+++ conteo/analisis_conteo_javi.py
@@ -17,26 +17,27 @@ variables = Parser(configuration = 'conteo').config()
 
 #GRAFICO FIGURA 4 PAPER
 
-path_ruido = './input/conteo/bose(50ns)/ruido_2v'
 tensiones_ruido = []
-for filename in os.listdir(path_ruido):
-    medicion = load_dict(fname = path_ruido + '/' + filename)
-    tensiones_ruido.append(medicion['tension_picos'])
-tensiones_ruido = np.concatenate(tensiones_ruido)
+carpeta = 'input/conteo/bose(50ns)/ruido_2v/'
+for f in os.listdir(os.path.join(carpeta)):
+    medicion = load_dict(fname = os.path.join(carpeta + f))
+    for t in medicion['tension_picos']:
+        tensiones_ruido.append(t)
+tensiones_ruido, cuentas_ruido = np.unique(tensiones_ruido, return_counts = True)
 
-path = './input/conteo/bose(50ns)/laser_2v'
 tensiones = []
-for filename in os.listdir(path):
-    medicion = load_dict(fname = path + '/' + filename)
-    tensiones.append(medicion['tension_picos'])
-tensiones = np.concatenate(tensiones)
+carpeta = './input/conteo/bose(50ns)/laser_2v/'
+for f in os.listdir(carpeta):
+    medicion = load_dict(fname = os.path.join(carpeta + f))
+    for t in medicion['tension_picos']:
+        tensiones.append(t)
+tensiones, cuentas = np.unique(tensiones, return_counts = True)
 
 
 
-
-paso = 0.00020
-maximo_altura = 0.015
-bins = np.arange(-maximo_altura, 0, paso)
+# paso = 0.00020
+# maximo_altura = 0.015
+# bins = np.arange(-maximo_altura, 0, paso)
 
 umbral = -0.003
 
@@ -50,16 +51,18 @@ axs[0].legend(loc='upper left')
 axs[0].set_ylabel('Tiempo [$\mu$s]')
 axs[0].grid(visible = True, alpha=0.3)
 
-axs[1].hist(tensiones*1e3,
-          label = "Laser prendido",
-          bins = bins*1e3,
-        #   histtype = "step", 
-          color = "C1")
-axs[1].hist(tensiones_ruido*1e3,
-         bins = bins*1e3,
-         label = "Ruido",
-        #  histtype = "step", 
-         color = "C2")
+axs[1].bar(tensiones*1e3,
+        cuentas,
+        color = 'C1', 
+        width= .25,
+        alpha = .75,
+        label = 'Laser incidiendo sobre FM')
+axs[1].bar(tensiones_ruido*1e3,
+        cuentas_ruido,
+        color = 'C0', 
+        width= .25,
+        alpha = 1,
+        label = 'Ruido')
 axs[1].set_yscale('log')
 ylim = axs[1].get_ylim()
 axs[1].vlines(umbral*1e3, -10, 10e5, linestyles='dashed', colors='black')
@@ -68,7 +71,7 @@ axs[1].set_xlabel('Tensión [mV]')
 axs[1].set_ylabel('Número de eventos')
 axs[1].grid(visible = True, alpha=0.3)
 axs[1].set_ylim(ylim)
-# plt.savefig('./output/conteo/ruido-umbral-ej.png', dpi=400)
+plt.savefig('./output/conteo/presentacion/ruido-umbral-ej.png', dpi=400)
 plt.show()
 
 ########################################
@@ -148,7 +151,8 @@ def Pn_teo_poisson(t, n, nT, T):
   return (nT/T*t)**n / np.math.factorial(n) * np.exp(-nT/T*t)
 
 def error_formula_poisson(a_0, x, n, T):
-    return (n/(a_0*x/T) -1)*np.exp(-a_0*x/T)*(a_0*x/T)**n/np.math.factorial(n)
+    # return (n/(a_0*x/T) -1)*np.exp(-a_0*x/T)*(a_0*x/T)**n/np.math.factorial(n)
+    return (np.exp(-a_0*x/T)*(a_0*x/T)**n * (n*T-a_0*x))/(T*a_0*np.math.factorial(n))
 
 
 
@@ -203,6 +207,7 @@ plt.ylabel('Probablidad de 0-fotones')
 plt.xlabel('Tamaño de ventana temporal [ns]')
 plt.grid()
 plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P0-bose.png', dpi=400)
 plt.show()
 
 plt.errorbar(vent, P1_bose_total, yerr=error_frecuencia1+P1_bose_total*0.01, fmt='.',label='Datos', markevery=70, errorevery=70, capsize=5, color='C0')
@@ -218,6 +223,7 @@ plt.ylabel('Probablidad de 1-fotón')
 plt.xlabel('Tamaño de ventana temporal [ns]')
 plt.grid()
 plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P1-bose.png', dpi=400)
 plt.show()
 
 plt.errorbar(vent, P2_bose_total, yerr=error_frecuencia2+P2_bose_total*0.01, fmt='.',label='Datos', markevery=70, errorevery=70, capsize=5, color='C0')
@@ -233,6 +239,7 @@ plt.ylabel('Probablidad de 2-fotones')
 plt.xlabel('Tamaño de ventana temporal [ns]')
 plt.grid()
 plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P2-bose.png', dpi=400)
 plt.show()
 
 
@@ -240,17 +247,19 @@ plt.show()
 #POISSON
 ########################################
 
-path_poisson = 'input\\conteo\\poisson(10ms)\\laser_2v'
+path_poisson = 'input\\conteo\\poisson(10ms)\\laser_2v_bis_2' #Ventana de 500 ns
 
-umbral_poisson = 3.1
+umbral_poisson = 3.1 #1
 P0_poisson_total = np.zeros(2500)
 P1_poisson_total = np.zeros(2500)
 P2_poisson_total = np.zeros(2500)
-for filename in os.listdir(path_poisson):
+ocurrencias = np.zeros((2500, 1000))
+for j, filename in enumerate(os.listdir(path_poisson)):
   data = load_dict(path_poisson + '\\' + filename)
   P0_poisson = np.zeros(2500)
   P1_poisson = np.zeros(2500)
   P2_poisson = np.zeros(2500)
+  ocurrencia_tau = np.zeros(2500)
   for i in range(2500):
     if i == 0:
       indice_picos = find_peaks(-data['tension'], height = umbral_poisson)[0]#, distance = 50)[0]
@@ -262,40 +271,115 @@ for filename in os.listdir(path_poisson):
       tension_picos = data['tension'][:-i][indice_picos]
   
     ocurrencia = len(tension_picos[tension_picos<-umbral_poisson])
+    ocurrencia_tau[i] = ocurrencia
     if ocurrencia == 0:
       P0_poisson[i] += 1
     elif ocurrencia == 1:
       P1_poisson[i] += 1 
     elif ocurrencia == 2:
       P2_poisson[i] += 1
+
+  ocurrencias[:,j] = ocurrencia_tau
+  
   P0_poisson_total += P0_poisson
   P1_poisson_total += P1_poisson
   P2_poisson_total += P2_poisson 
 
+error_frecuencia0 = np.zeros(2500)
+error_frecuencia1 = np.zeros(2500)
+error_frecuencia2 = np.zeros(2500)
+for i in range(2500):
+  cuentas, apariciones = np.unique(ocurrencias[i,:], return_counts=True)
+  error_suma_apariciones = np.sqrt(np.sum(apariciones))
+  frecuencia = apariciones/np.sum(apariciones)
+  try:
+    error_frecuencia0[i], error_frecuencia1[i], error_frecuencia2[i], *_ = np.sqrt((np.sqrt(apariciones)/np.sum(apariciones))**2 + 
+          (apariciones*error_suma_apariciones/(np.sum(apariciones)**2))**2)
+  except:
+    try:
+      error_frecuencia0[i], error_frecuencia1[i], *_ = np.sqrt((np.sqrt(apariciones)/np.sum(apariciones))**2 + 
+        (apariciones*error_suma_apariciones/(np.sum(apariciones)**2))**2)
+    except:
+      error_frecuencia0[i], *_ = np.sqrt((np.sqrt(apariciones)/np.sum(apariciones))**2 + 
+      (apariciones*error_suma_apariciones/(np.sum(apariciones)**2))**2)
 
-n_mean = 1 #A CHEQUEAR
+P0_poisson_total = P0_poisson_total/1000
+P1_poisson_total = P1_poisson_total/1000
+P2_poisson_total = P2_poisson_total/1000
+
+n_mean = 0.8021739 #20.06304 
+n_std = 0.05725898 #0.9861
 T = 100 #ms
+vent = np.linspace(100, 0, 2500) #ms
 
-fig, axs = plt.subplots(3, sharex=True)
-vent = np.linspace(T, 0, 2500) #ms
-axs[0].plot(vent, P0_poisson_total/np.max(P0_poisson_total), 'o',label='Datos', markevery=70)
-# axs[0].plot(vent, Pn_teo_bose(vent, 0, n_mean, T), label='Bose Einstein')
-# axs[0].plot(vent, Pn_teo_poisson(vent, 0, n_mean, T), label='Poisson')
-axs[0].set_ylabel('$P_0$')
-axs[0].grid()
-axs[0].legend()
+franja_error0_bose = Propagacion_errores(
+        variables = [('a_0', n_mean), ('T', T)], 
+        errores = np.array([n_std, 0]).reshape(-1,1), 
+        formula = formula_bose_str0, 
+        dominio = vent
+        ).fit()[1]
+franja_error1_bose = Propagacion_errores(
+        variables = [('a_0', n_mean), ('T', T)], 
+        errores = np.array([n_std, 0]).reshape(-1,1), 
+        formula = formula_bose_str1, 
+        dominio = vent
+        ).fit()[1]
+franja_error2_bose = Propagacion_errores(
+        variables = [('a_0', n_mean), ('T', T)], 
+        errores = np.array([n_std, 0]).reshape(-1,1), 
+        formula = formula_bose_str2, 
+        dominio = vent
+        ).fit()[1]
+franja_error0_poisson = np.sqrt((error_formula_poisson(n_mean, vent, 0, T)*n_std)**2)
+franja_error1_poisson = np.sqrt((error_formula_poisson(n_mean, vent, 1, T)*n_std)**2)
+franja_error2_poisson = np.sqrt((error_formula_poisson(n_mean, vent, 2, T)*n_std)**2)
 
-axs[1].plot(vent, P1_poisson_total/np.max(P1_poisson_total), 'o', markevery=70)
-# axs[1].plot(vent, Pn_teo_bose(vent, 1, n_mean, T))
-# axs[1].plot(vent, Pn_teo_poisson(vent, 1, n_mean, T))
-axs[1].set_ylabel('$P_1$')
-axs[1].grid()
 
-axs[2].plot(vent, P2_poisson_total/np.max(P2_poisson_total), 'o', markevery=70)
-# axs[2].plot(vent, Pn_teo_bose(vent, 2, n_mean, T))
-# axs[2].plot(vent, Pn_teo_poisson(vent, 2, n_mean, T))
-axs[2].set_ylabel('$P_2$')
-axs[2].grid()
-axs[2].set_xlabel('Tamaño de ventana temporal [ms]')
 
+plt.errorbar(vent, P0_poisson_total, yerr=error_frecuencia0+P0_poisson_total*0.01, fmt='.',label='Datos', markevery=70, errorevery=70, capsize=2, zorder=5)
+plt.plot(vent, Pn_teo_bose(vent, 0, n_mean, T), label='Bose Einstein', color='C1')
+plt.plot(vent, Pn_teo_bose(vent, 0, n_mean, T) + franja_error0_bose, '--', color = 'C1')
+plt.plot(vent, Pn_teo_bose(vent, 0, n_mean, T) - franja_error0_bose, '--', color = 'C1')
+plt.fill_between(vent, Pn_teo_bose(vent, 0, n_mean, T) - franja_error0_bose, Pn_teo_bose(vent, 0, n_mean, T) + franja_error0_bose, facecolor = "gray", alpha = 0.3)
+plt.plot(vent, Pn_teo_poisson(vent, 0, n_mean, T), label='Poisson', color='C2')
+plt.plot(vent, Pn_teo_poisson(vent, 0, n_mean, T) + franja_error0_poisson, '--', color = 'C2')
+plt.plot(vent, Pn_teo_poisson(vent, 0, n_mean, T) - franja_error0_poisson, '--', color = 'C2')
+plt.fill_between(vent, Pn_teo_poisson(vent, 0, n_mean, T) - franja_error0_poisson, Pn_teo_poisson(vent, 0, n_mean, T) + franja_error0_poisson, facecolor = "gray", alpha = 0.3)
+plt.ylabel('Probablidad de 0-fotones')
+plt.xlabel('Tamaño de ventana temporal [ms]')
+plt.grid()
+plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P0-poisson-umbral-grande.png', dpi=400)
+plt.show()
+
+plt.errorbar(vent, P1_poisson_total, yerr=error_frecuencia1+P1_poisson_total*0.01, fmt='.',label='Datos', markevery=70, errorevery=70, capsize=5, color='C0')
+plt.plot(vent, Pn_teo_bose(vent, 1, n_mean, T), label='Bose Einstein', color='C1')
+plt.plot(vent, Pn_teo_bose(vent, 1, n_mean, T) + franja_error1_bose, '--', color = 'C1')
+plt.plot(vent, Pn_teo_bose(vent, 1, n_mean, T) - franja_error1_bose, '--', color = 'C1')
+plt.fill_between(vent, Pn_teo_bose(vent, 1, n_mean, T) - franja_error1_bose, Pn_teo_bose(vent, 1, n_mean, T) + franja_error1_bose, facecolor = "gray", alpha = 0.3)
+plt.plot(vent, Pn_teo_poisson(vent, 1, n_mean, T), label='Poisson', color='C2')
+plt.plot(vent, Pn_teo_poisson(vent, 1, n_mean, T) + franja_error1_poisson, '--', color = 'C2')
+plt.plot(vent, Pn_teo_poisson(vent, 1, n_mean, T) - franja_error1_poisson, '--', color = 'C2')
+plt.fill_between(vent, Pn_teo_poisson(vent, 1, n_mean, T) - franja_error1_poisson, Pn_teo_poisson(vent, 1, n_mean, T) + franja_error1_poisson, facecolor = "gray", alpha = 0.3)
+plt.ylabel('Probablidad de 1-fotón')
+plt.xlabel('Tamaño de ventana temporal [ms]')
+plt.grid()
+plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P1-poisson-umbral-grande.png', dpi=400)
+plt.show()
+
+plt.errorbar(vent, P2_poisson_total, yerr=error_frecuencia2+P2_bose_total*0.01, fmt='.',label='Datos', markevery=70, errorevery=70, capsize=5, color='C0')
+plt.plot(vent, Pn_teo_bose(vent, 2, n_mean, T), label='Bose Einstein', color='C1')
+plt.plot(vent, Pn_teo_bose(vent, 2, n_mean, T) + franja_error2_bose, '--', color = 'C1')
+plt.plot(vent, Pn_teo_bose(vent, 2, n_mean, T) - franja_error2_bose, '--', color = 'C1')
+plt.fill_between(vent, Pn_teo_bose(vent, 2, n_mean, T) - franja_error2_bose, Pn_teo_bose(vent, 2, n_mean, T) + franja_error2_bose, facecolor = "gray", alpha = 0.3)
+plt.plot(vent, Pn_teo_poisson(vent, 2, n_mean, T), label='Poisson', color='C2')
+plt.plot(vent, Pn_teo_poisson(vent, 2, n_mean, T) + franja_error2_poisson, '--', color = 'C2')
+plt.plot(vent, Pn_teo_poisson(vent, 2, n_mean, T) - franja_error2_poisson, '--', color = 'C2')
+plt.fill_between(vent, Pn_teo_poisson(vent, 2, n_mean, T) - franja_error2_poisson, Pn_teo_poisson(vent, 2, n_mean, T) + franja_error2_poisson, facecolor = "gray", alpha = 0.3)
+plt.ylabel('Probablidad de 2-fotones')
+plt.xlabel('Tamaño de ventana temporal [ms]')
+plt.grid()
+plt.legend()
+plt.savefig('output\\conteo\\presentacion\\P2-poisson-umbral-grande.png', dpi=400)
 plt.show()
